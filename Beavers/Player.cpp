42f0a03b8a876f72b05638bd6b5684b5662fb6f2
Dropbearzzz <146158177+Dropbearzzz@users.noreply.github.com>
2024-08-15 15:56:22 +1200

    Added Basic Animation

--- Beavers/Player.cpp
+++ Beavers/Player.cpp
@@ -9,7 +9,7 @@
 */
 Player::Player(Vec2f _position, shared_ptr<b2World> _world) : Object(_position, "Resources/Images/Entities/Player.png", _world, false)
 {
-	SetDrawRect(sf::IntRect(0, 0, 16, 16));
+	SetDrawRect(sf::IntRect(0, 16, 16, 16));
 	AddBoxCollider(Vec2f(0, 6), Vec2f(16, 2), false);
 }
 
@@ -21,6 +21,21 @@ Player::Player(Vec2f _position, shared_ptr<b2World> _world) : Object(_position, 
 */
 void Player::Update(float _fDeltaTime)
 {
+	// Updates Current Animation Frame
+	if (m_animationClock.getElapsedTime().asSeconds() > 3.0f / 24.0f)
+	{
+		m_animationClock.restart();
+		m_iAnimationFrame++;
+
+		if (m_iAnimationFrame > 3)
+		{
+			m_iAnimationFrame = 0;
+		}
+
+		m_sprite.setTextureRect(sf::IntRect(16 * m_iAnimationFrame, 16, 16, 16));
+	}
+
+	// Handles Player Movement
 	sf::Vector2f displacement;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 	{
@@ -29,6 +44,7 @@ void Player::Update(float _fDeltaTime)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
 		displacement += Vec2f(-1.0f, 0.0f);
+		m_sprite.setScale(-1, 1);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 	{
@@ -37,6 +53,7 @@ void Player::Update(float _fDeltaTime)
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
 		displacement += Vec2f(1.0f, 0.0f);
+		m_sprite.setScale(1, 1);
 	}
 
 	ApplyForce(displacement * _fDeltaTime * 1000.0f * m_fSpeed);
--- Beavers/Player.h
+++ Beavers/Player.h
@@ -5,11 +5,13 @@ class Player : public Object
 {
 public:
 	Player(Vec2f _position, shared_ptr<b2World> _world);
-
-	void Move(Vec2f _moveDir, float _fDeltaTime);
+	
 	void Update(float _fDeltaTime) override;
 
 private:
+	sf::Clock m_animationClock;
 	float m_fSpeed = 30.0f;
+
+	int m_iAnimationFrame = 0;
 };
 
